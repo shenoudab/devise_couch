b45f416121dd159aa35644aa7be08a189fde73d2
Shenouda Bertel <ShenoudaB@gmail.com>
2010-11-03 01:48:48 +0200

    Modify Schema for registration edit

--- lib/devise/orm/couchrest_model/schema.rb
+++ lib/devise/orm/couchrest_model/schema.rb
@@ -15,8 +15,14 @@ module Devise
 
         def find(*args)
           options = args.extract_options!
-          raise "You can't search with more than one condition yet =(" if options[:conditions].keys.size > 1
-          find_by_key_and_value(options[:conditions].keys.first, options[:conditions].values.first)
+
+          if options.present?
+            raise "You can't search with more than one condition yet =(" if options[:conditions].keys.size > 1
+            find_by_key_and_value(options[:conditions].keys.first, options[:conditions].values.first)
+          else
+            id = args.flatten.compact.uniq.join
+            find_by_key_and_value(:id, id)
+          end
         end
         
         private
